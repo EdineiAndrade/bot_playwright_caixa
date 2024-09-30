25f0a7330290255a16dde414ef8dc9e586f1a9fd
EdineiAndrade <edinei22.ba@gmail.com>
2024-09-30 00:05:42 -0300

    ajuste, erro - fgts

--- bot_caixa.py
+++ bot_caixa.py
@@ -62,78 +62,77 @@ def busca_site_caixa(page, name_file, df):
     nome_excel = name_file.replace('csv', 'xlsx')
         
     for index, row in df.iterrows():
+        try:
+            now = datetime.now()
+            formatted_date = now.strftime("%d/%m/%y %H:%M:%S")        
+            # Cálculo de quantas consultas faltam
+            faltam = numero_de_linhas - (index + 1)        
+            # Tempo decorrido
+            elapsed_time = now - start_time
+            link = row['Link de acesso']
+            page.goto(link)
 
-        now = datetime.now()
-        formatted_date = now.strftime("%d/%m/%y %H:%M:%S")        
-        # Cálculo de quantas consultas faltam
-        faltam = numero_de_linhas - (index + 1)        
-        # Tempo decorrido
-        elapsed_time = now - start_time
-        link = row['Link de acesso']
-        page.goto(link)
+            seletor_1 = '//*[@class="related-box"]/span/i'
 
-        seletor_1 = '//*[@class="related-box"]/span/i'
+            count = page.locator(seletor_1).count()
 
-        count = page.locator(seletor_1).count()
-
-        if count > 0:
-            if count >= 2:
-                leilao_01 = page.locator('//*[@class="related-box"]/span[4]').inner_text()
-                leilao_02 = page.locator('//*[@class="related-box"]/span[5]').inner_text()
-            elif count == 1:
-                leilao_01 = page.locator('//*[@class="related-box"]/span[4]').inner_text()
+            if count > 0:
+                if count >= 2:
+                    leilao_01 = page.locator('//*[@class="related-box"]/span[4]').inner_text()
+                    leilao_02 = page.locator('//*[@class="related-box"]/span[5]').inner_text()
+                elif count == 1:
+                    leilao_01 = page.locator('//*[@class="related-box"]/span[4]').inner_text()
+                    leilao_02 = 'sem informações'
+            else:
+                leilao_01 = 'sem informações'
                 leilao_02 = 'sem informações'
-        else:
-            leilao_01 = 'sem informações'
-            leilao_02 = 'sem informações'
 
-        fgts_info = page.locator('//*[@class="related-box"]/p[3]').inner_text()
-        fgts_info = fgts_info.replace('\xa0', '')
-        fgts_info = fgts_info.split('\n')
+            fgts_info = page.locator('//*[@class="related-box"]/p[3]').inner_text()
+            fgts_info = fgts_info.replace('\xa0', '')
+            fgts_info = fgts_info.split('\n')
 
-        for info in enumerate(fgts_info):
-            fgts = "SIM"
-            if "Imóvel NÃO aceita utilização de FGTS" in str(info):
-                fgts = "NÃO"
-            if "Permite financiamento na linha de crédito SBPE (Consulte Condições)" in str(info) or "Imovel ACEITA financiamento" in str(info):
-                financiamento = "SIM"
-            if "Imóvel NÃO aceita financiamento habitacional" in str(info):
-                financiamento = "NÃO"
-            if "Imóvel NÃO aceita parcelamento" in str(info):
-                parcelamento = "NÃO"
-            if " Imóvel ACEITA  parcelamento" in str(info):
-                parcelamento = "SIM"
+            for info in enumerate(fgts_info):
+                fgts = "SIM"
+                if "Imóvel NÃO aceita utilização de FGTS" in str(info):
+                    fgts = "NÃO"
+                if "Permite financiamento na linha de crédito SBPE (Consulte Condições)" in str(info) or "Imovel ACEITA financiamento" in str(info):
+                    financiamento = "SIM"
+                if "Imóvel NÃO aceita financiamento habitacional" in str(info):
+                    financiamento = "NÃO"
+                if "Imóvel NÃO aceita parcelamento" in str(info):
+                    parcelamento = "NÃO"
+                if " Imóvel ACEITA  parcelamento" in str(info):
+                    parcelamento = "SIM"
 
-            if "Imóvel NÃO aceita consórcio" in str(info):
-                consorcio = "NÃO"
-            if " Imóvel ACEITA  consórcio" in str(info):
-                consorcio = "SIM"
+                if "Imóvel NÃO aceita consórcio" in str(info):
+                    consorcio = "NÃO"
+                if " Imóvel ACEITA  consórcio" in str(info):
+                    consorcio = "SIM"
 
-        df.at[index, 'Banco'] = 'Caixa'
-        df.at[index, 'Acesso'] = " "
-        df.at[index, 'FGTS'] = fgts
-        df.at[index, 'Financiamento'] = financiamento
-        df.at[index, 'Parcelamento'] = parcelamento
-        df.at[index, 'Consórcio'] = consorcio
-        df.at[index, '1° Leilão'] = leilao_01
-        df.at[index, '2° Leilão'] = leilao_02
+            df.at[index, 'Banco'] = 'Caixa'
+            df.at[index, 'Acesso'] = " "
+            df.at[index, 'FGTS'] = fgts
+            df.at[index, 'Financiamento'] = financiamento
+            df.at[index, 'Parcelamento'] = parcelamento
+            df.at[index, 'Consórcio'] = consorcio
+            df.at[index, '1° Leilão'] = leilao_01
+            df.at[index, '2° Leilão'] = leilao_02
 
-        
-        # Estimativa de tempo restante
-        if index > 0:  # Evitar divisão por zero
-            estimated_total_time = elapsed_time / (index + 1) * numero_de_linhas
-            remaining_time = estimated_total_time - elapsed_time
-        else:
-            remaining_time = None  # Sem estimativa no primeiro loop
+            # Estimativa de tempo restante
+            if index > 0:  # Evitar divisão por zero
+                estimated_total_time = elapsed_time / (index + 1) * numero_de_linhas
+                remaining_time = estimated_total_time - elapsed_time
+            else:
+                remaining_time = None  # Sem estimativa no primeiro loop
 
-        if (now - ultimo_salvamento) >= timedelta(minutes=30):
-            df.to_excel(nome_excel)    # Chama a função para salvar o Excel
-            ultimo_salvamento = now
+            if (now - ultimo_salvamento) >= timedelta(minutes=30):
+                df.to_excel(nome_excel)    # Chama a função para salvar o Excel
+                ultimo_salvamento = now
 
-        # Chama a função para imprimir os dados
-        imprimir_consulta(index, formatted_date, faltam, elapsed_time, remaining_time)
-        
-            
+            # Chama a função para imprimir os dados
+            imprimir_consulta(index, formatted_date, faltam, elapsed_time, remaining_time)
+        except Exception as e:
+            continue
     nome_excel = name_file.replace('csv', 'xlsx')
     df.to_excel(nome_excel)       
 
